5e275e3ef7dbc1e2819df0d188fe161e942293ed
Dylan Krupp <dylan.krupp@promessinc.com>
2021-03-02 11:57:12 -0500

    -Continued implementation

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    body1: {
+      color: 'hsl(0, 0%, 63%)',
+      fontSize: '14px'
+    },
+    h4: {
+      fontWeight: 700,
+      color: 'hsl(0, 0%, 0%)',
+      fontSize: '40px'
+    },
+    fontFamily: [
+      'Spartan',
+      'sans-serif'
+    ].join(',')
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme} >
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
